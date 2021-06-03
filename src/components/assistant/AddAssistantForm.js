019e3237abf7e7e8858abbd014f968ed5ef962d0
Liz Kamugisha <61685152+LizKamugisha@users.noreply.github.com>
2021-06-03 14:33:05 +0300

    Update AddAssistantForm.js

--- src/components/assistant/AddAssistantForm.js
+++ src/components/assistant/AddAssistantForm.js
@@ -45,44 +45,44 @@ const handleSubmit = (e) => {
 };
 
 return (
-<Form onSubmit={handleSubmit}>
-  <Form.Group as={Row} controlId="formFirstName">
-    <Form.Label column sm={3}>
-      First Name:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control 
-        type="text" 
-         value={assistant.firstName}
-         onChange={(e) => {
+  <Form onSubmit={handleSubmit}>
+    <Form.Group as={Row} controlId="formFirstName">
+      <Form.Label column sm={3}>
+        First Name:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
+          type="text"
+          value={assistant.firstName}
+          onChange={(e) => {
             setAssistant({ ...assistant, firstName: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formSurname">
-    <Form.Label column sm={3}>
-      Surname:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control
+    <Form.Group as={Row} controlId="formSurname">
+      <Form.Label column sm={3}>
+        Surname:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
           type="text"
           value={assistant.surName}
           onChange={(e) => {
             setAssistant({ ...assistant, surName: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formGender">
-    <Form.Label column sm={3}>
-      Select Gender:
-    </Form.Label>
-        {['radio'].map((type) => (
-            <Col key={`inline-${type}`} sm={8}>
-            <Form.Check
+    <Form.Group as={Row} controlId="formGender">
+      <Form.Label column sm={3}>
+        Select Gender:
+      </Form.Label>
+      {["radio"].map((type) => (
+        <Col key={`inline-${type}`} sm={8}>
+          <Form.Check
             inline
             label="Male"
             name="gender"
@@ -92,9 +92,9 @@ return (
             onChange={(e) => {
               setAssistant({ ...assistant, gender: e.target.value });
             }}
-            />
-             <Form.Check
-             inline
+          />
+          <Form.Check
+            inline
             label="Female"
             name="gender"
             type={type}
@@ -104,121 +104,121 @@ return (
               setAssistant({ ...assistant, gender: e.target.value });
             }}
           />
-            </Col>
-        ))}
-  </Form.Group>
+        </Col>
+      ))}
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formPhone">
-    <Form.Label column sm={3}>
-      Phone Number:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control
+    <Form.Group as={Row} controlId="formPhone">
+      <Form.Label column sm={3}>
+        Phone Number:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
           type="number"
           value={assistant.phone}
           onChange={(e) => {
             setAssistant({ ...assistant, phone: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formAddress">
-    <Form.Label column sm={3}>
-      Home Address:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control
+    <Form.Group as={Row} controlId="formAddress">
+      <Form.Label column sm={3}>
+        Home Address:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
           type="text"
           value={assistant.address}
           onChange={(e) => {
             setAssistant({ ...assistant, address: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formMarket">
-    <Form.Label column sm={3}>
-      Select Market:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control as="select">
+    <Form.Group as={Row} controlId="formMarket">
+      <Form.Label column sm={3}>
+        Select Market:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control as="select">
           <option>Gabba</option>
           <option>Nakasero</option>
           <option>Kikubo</option>
           <option>Owino</option>
-           value={assistant.market}
+          value={assistant.market}
           onChange=
           {(e) => {
             setAssistant({ ...assistant, market: e.target.value });
           }}
         </Form.Control>
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-<Form.Group as={Row} controlId="formReferalNum">
-    <Form.Label column sm={3}>
-      Referal Number:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control
+    <Form.Group as={Row} controlId="formReferalNum">
+      <Form.Label column sm={3}>
+        Referal Number:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
           type="text"
           value={assistant.refNumber}
           onChange={(e) => {
             setAssistant({ ...assistant, refNumber: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formProfilePhoto">
-    <Form.Label column sm={3}>
-      Profile Photo:
-    </Form.Label>
-    <Col sm={8}>
-        <Form.File 
-        id="custom-file"
-        label="Upload your profile photo"
-        custom
-        onChange={(e) => {
+    <Form.Group as={Row} controlId="formProfilePhoto">
+      <Form.Label column sm={3}>
+        Profile Photo:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.File
+          id="custom-file"
+          label="Upload your profile photo"
+          custom
+          onChange={(e) => {
             setAssistant({ ...assistant, profile_pic: e.target.files[0] });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formPassword">
-    <Form.Label column sm={3}>
-      Create Password:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control 
-      type="password" 
-      value={assistant.password}
+    <Form.Group as={Row} controlId="formPassword">
+      <Form.Label column sm={3}>
+        Create Password:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
+          type="password"
+          value={assistant.password}
           onChange={(e) => {
             setAssistant({ ...assistant, password: e.target.value });
           }}
-      />
-    </Col>
-  </Form.Group>
+        />
+      </Col>
+    </Form.Group>
 
-  <Form.Group as={Row} controlId="formPassword">
-    <Form.Label column sm={3}>
-      Confirm Password:
-    </Form.Label>
-    <Col sm={8}>
-      <Form.Control
+    <Form.Group as={Row} controlId="formPassword">
+      <Form.Label column sm={3}>
+        Confirm Password:
+      </Form.Label>
+      <Col sm={8}>
+        <Form.Control
           type="password"
           value={assistant.confirm_password}
           onChange={(e) => {
             setAssistant({ ...assistant, confirm_password: e.target.value });
           }}
         />
-    </Col>
-  </Form.Group>
+      </Col>
+    </Form.Group>
 
-     <Form.Group as={Row} controlId="formEmail">
+    <Form.Group as={Row} controlId="formEmail">
       <Form.Label column sm={3}>
         Email:
       </Form.Label>
@@ -232,16 +232,16 @@ return (
         />
       </Col>
     </Form.Group>
-    
+
     <Form.Group as={Row}>
-        <Col sm={{ span: 8, offset: 3 }}>
-        <Button type="submit" style={{backgroundColor:"#E78C06"}} > 
-        {" "}
-        Register{" "}
+      <Col sm={{ span: 8, offset: 3 }}>
+        <Button type="submit" style={{ backgroundColor: "#E78C06" }}>
+          {" "}
+          Register{" "}
         </Button>
-        </Col>
+      </Col>
     </Form.Group>
-    </Form>
+  </Form>
 );
 };
 export default AddAssistantForm;
